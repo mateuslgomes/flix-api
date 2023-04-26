3ad5835af20946dd9d84a8e763378db1a40b701a
mateuslgomes <mateuslgomes.dev@gmail.com>
2023-04-26 20:21:21 -0300

    criando projeto

---
+++ api/src/test/java/br/com/flix/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.flix.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
